d85489166ecd7614d69691408f7e32bf5bed1de9
Gabriel Rodrigues <gabrielrodrigues62902@gmail.com>
2024-09-18 15:01:01 -0300

    commit teste

---
+++ coperagri/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+        <Link href='/routes'>teste</Link>
+    </div>
+  );
+}
