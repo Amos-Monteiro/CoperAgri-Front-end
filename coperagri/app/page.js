9d76480190e226e41d806d7737accb2e858cd27e
Gabriel Rodrigues <gabrielrodrigues62902@gmail.com>
2024-09-21 09:52:37 -0300

    Login finalizado

--- coperagri/app/page.js
+++ coperagri/app/page.js
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
+    <div className={styles.teste}>
         <Link href='/routes/login'>Login</Link>
     </div>
   );
